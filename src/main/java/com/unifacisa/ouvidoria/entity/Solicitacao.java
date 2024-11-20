23206273db9d6c3e1d4df248235167623cf7e2ca
LH-Assis <luiz.henri664@gmail.com>
2024-11-20 18:19:52 -0300

    Implementando o LOMBOK e Swagger

--- src/main/java/com/unifacisa/ouvidoria/entity/Solicitacao.java
+++ src/main/java/com/unifacisa/ouvidoria/entity/Solicitacao.java
@@ -12,7 +12,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+//Luiz implementando LOMBOK
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name="solicitacao")
 public class Solicitacao {
@@ -33,63 +42,8 @@ public class Solicitacao {
     @Enumerated(EnumType.STRING)
     private Status status;
 
-	public String getResposta() {
-		return resposta;
-	}
-
-	public void setResposta(String resposta) {
-		this.resposta = resposta;
-	}
-
-	public Status getStatus() {
-		return status;
-	}
-
-	public void setStatus(Status status) {
-		this.status = status;
-	}
-
 	@ManyToOne
     @JoinColumn(name = "usuario_id")
     private Usuario usuario;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public LocalDateTime getDataCriacao() {
-		return dataCriacao;
-	}
-
-	public void setDataCriacao(LocalDateTime dataCriacao) {
-		this.dataCriacao = dataCriacao;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}  
 }
